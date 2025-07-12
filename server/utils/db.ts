676de2e41510c6356bfd20294e55f3d74dae26fa
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-07-12 16:46:33 +0200

    scaffolding db utils

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv";
 import { Db, MongoClient } from "mongodb";
 
-export async function getDbConnection(): Promise<Db> {
+export async function getDb(): Promise<Db> {
+    var connection = await getConnection();
+    return connection.db("menu")
+}
+
+export async function getConnection(): Promise<MongoClient> {
     dotenv.config();
-
     const client: MongoClient = new MongoClient(process.env.MONGO_URI!);
-
     await client.connect();
-
-    return client.db("menu")
+    return client;
 }
