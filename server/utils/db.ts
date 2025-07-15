22d34fb66f51cc34e07b26c74768049b24473b26
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-07-15 21:36:37 +0200

    improved memory server performance

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,7 +1,6 @@
 import { Db, MongoClient } from "mongodb";
 import { loadEnv } from "./functions";
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import test from "node:test";
 
 export async function getDb(): Promise<Db> {
     var connection = await getConnection();
@@ -14,15 +13,15 @@ export async function getConnection(): Promise<MongoClient> {
     loadEnv()
     if (process.env.NODE_ENV && process.env.NODE_ENV === "test") {
 
-        const mongod = await MongoMemoryServer.create({
-            instance: {
-                dbName: process.env.MONGO_DB_NAME,
-            }
-        });
-
         if (memoryServer) {
             return memoryServer;
         } else {
+            const mongod = await MongoMemoryServer.create({
+                instance: {
+                    dbName: process.env.MONGO_DB_NAME,
+                }
+            });
+
             memoryServer = new MongoClient(mongod.getUri());
             await memoryServer.connect();
             return memoryServer;
